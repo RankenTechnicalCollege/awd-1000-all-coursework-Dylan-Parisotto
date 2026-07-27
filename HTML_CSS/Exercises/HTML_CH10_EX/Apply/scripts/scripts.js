63013227be8ceb381f8ac4f3d4d91d82ba161841
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-07-27 16:21:58 -0500

    ch 10 apply

---
+++ HTML_CSS/Exercises/HTML_CH10_EX/Apply/scripts/scripts.js
@@ -0,0 +1,12 @@
+//name: Dylan Parisotto
+//file name: script.js
+//date:7/27/2026
+
+//function to display content
+
+function content() {
+  var text = document.getElementById("new") ;
+  text.textContent = "Hi. I am a new paragraph that was created with a javascript function." ;
+  text.style.color = "#c0145f";
+  text.style.fontSize = "2em";
+}
