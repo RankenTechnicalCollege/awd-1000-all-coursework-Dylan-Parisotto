1bd25f3368ce260ac853a4854504d5060d213041
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-07-27 17:05:30 -0500

    ch 10 extend

---
+++ HTML_CSS/Exercises/HTML_CH10_EX/Extend/scripts/script.js
@@ -0,0 +1,16 @@
+/*
+    Student Name: Dylan Parisotto
+    File Name: script.js
+    Date: 7/27/2026
+*/
+
+$(document).ready(function () {
+    $("#hide-btn").click(function () {
+        $("#main p").hide();
+    });
+
+    $("#show-btn").click(function () {
+        $("#main p").show();
+    });
+});
+
