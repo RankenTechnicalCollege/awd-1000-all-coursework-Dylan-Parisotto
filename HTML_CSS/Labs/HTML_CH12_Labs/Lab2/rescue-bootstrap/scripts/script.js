1d40dd0febdd0a521280ab9746575995dc7763ef
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-07-28 15:55:43 -0500

    ch 12 lab 2

--- HTML_CSS/Labs/HTML_CH12_Labs/Lab2/rescue-bootstrap/scripts/script.js
+++ HTML_CSS/Labs/HTML_CH12_Labs/Lab2/rescue-bootstrap/scripts/script.js
@@ -1,5 +1,10 @@
 /*
-    Student Name:
+    Student Name: Dylan Parisotto
     File Name: script.js
-    Date: 
+    Date: 7/28/2026
 */
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
