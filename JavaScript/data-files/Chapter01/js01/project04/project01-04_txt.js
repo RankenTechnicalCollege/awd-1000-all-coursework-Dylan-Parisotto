8f3efa0227bb4c326c9b336f1ea20526f8b238aa
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-07-29 13:11:01 -0500

    hop 1-4

--- JavaScript/data-files/Chapter01/js01/project04/project01-04_txt.js
+++ JavaScript/data-files/Chapter01/js01/project04/project01-04_txt.js
@@ -2,11 +2,19 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Dylan Parisotto
+    Date:   7/29/2026
 
-    Filename: project01-04.js
+    Filename: project01-04_txt.js
 */
 
-//define variables for home and work addresses
+let homeStreet = "1 Main St.";
+let homeCity = "Sicilia";
+let homeState = "MA";
+let homeCode = "02103";
+
+let workStreet = "15 Oak Ln.";
+let workCity = "Central City";
+let workState = "MA";
+let workCode = "02104";
 
