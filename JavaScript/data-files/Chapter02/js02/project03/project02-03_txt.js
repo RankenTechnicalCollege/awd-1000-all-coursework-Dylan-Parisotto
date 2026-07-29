6c37b322250056a506e594ebe0cda91101f33132
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-07-29 14:34:37 -0500

    hop 2-3

--- JavaScript/data-files/Chapter02/js02/project03/project02-03_txt.js
+++ JavaScript/data-files/Chapter02/js02/project03/project02-03_txt.js
@@ -3,8 +3,32 @@
       Project 02-03
 
       Application to return the shape of a clicked object
-      Author: 
-      Date:   
+      Author: Dylan Parisotto
+      Date:   7/29/2026
 
-      Filename: project02-03.js
+      Filename: project02-03_txt.js
  */
+
+document.getElementById("square").onmouseover = function() {
+      document.getElementById("feedback").innerHTML = "You're hovering over the square.";
+};
+
+document.getElementById("square").onmouseout = function() {
+      document.getElementById("feedback").innerHTML = "";
+};
+
+document.getElementById("triangle").onmouseover = function() {
+      document.getElementById("feedback").innerHTML = "You're hovering over the triangle.";
+};
+
+document.getElementById("triangle").onmouseout = function() {
+      document.getElementById("feedback").innerHTML = "";
+};
+
+document.getElementById("circle").onmouseover = function() {
+      document.getElementById("feedback").innerHTML = "You're hovering over the circle.";
+};
+
+document.getElementById("circle").onmouseout = function() {
+      document.getElementById("feedback").innerHTML = "";
+};
