700a503ad5fbe5337c079ad1beff8d4aacfc9efb
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-07-29 14:15:37 -0500

    hop 2-1

--- JavaScript/data-files/Chapter02/js02/project01/project02-01_txt.js
+++ JavaScript/data-files/Chapter02/js02/project01/project02-01_txt.js
@@ -2,10 +2,27 @@
       Chapter 2
       Project 02-01
 
-      Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Celsius <-> Fahrenheit Converter
+      Author: Dylan Parisotto
+      Date:   7/29/2026
 
-      Filename: project02-01.js
+      Filename: project02-01_txt.js
  */
 
+     function FahrenheitToCelsius(degree) {
+            return (degree - 32) / 1.8;
+     }
+
+     function CelsiusToFahrenheit(degree) {
+            return (degree * 1.8) +32;
+     }
+
+     document.getElementById("cValue").onchange = function() {
+            var cDegree = document.getElementById("cValue").value;
+            document.getElementById("fValue").value = CelsiusToFahrenheit(cDegree);
+     };
+
+     document.getElementById("fValue").onchange = function() {
+            var fDegree = document.getElementById("fValue").value;
+            document.getElementById("cValue").value = FahrenheitToCelsius(fDegree);
+     };
