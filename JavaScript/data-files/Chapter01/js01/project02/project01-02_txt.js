98e1f611bb01f63dedc6f95d42152e6f9aae7625
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-07-29 12:24:14 -0500

    hop1-2

--- JavaScript/data-files/Chapter01/js01/project02/project01-02_txt.js
+++ JavaScript/data-files/Chapter01/js01/project02/project01-02_txt.js
@@ -2,10 +2,11 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Dylan Parisotto
+    Date:   7/29/2026
 
-    Filename: project01-02.js
+    Filename: project01-02_txt.js
 */
 
 //define variables for service name and service speed
+service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate", service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig"
