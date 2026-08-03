be90d81f0a325cf9cfdf65759a65d12e967dc229
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-03 18:28:34 -0500

    hop 5-3

--- JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
+++ JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
@@ -19,7 +19,7 @@ for (let n = sourceDoc.firstElementChild; n !== null; n = n.nextElementSibling) 
    if (n.nodeName === heading) {
       let anchor = document.createElement("a");
       anchor.name = "doclink" + headingCount;
-      n.insertBefore(anchor, n.firstChild);a
+      n.insertBefore(anchor, n.firstChild);
 
       let listItem = document.createElement("li");
       let link = document.createElement("a");
