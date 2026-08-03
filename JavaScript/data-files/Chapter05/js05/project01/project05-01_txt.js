1610264a775db25531af9a43c5968f73f3ccb1a9
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-03 18:07:07 -0500

    hop 5-1

--- JavaScript/data-files/Chapter05/js05/project01/project05-01_txt.js
+++ JavaScript/data-files/Chapter05/js05/project01/project05-01_txt.js
@@ -4,15 +4,15 @@
       Project 05-01
 
       Project to present an online quiz with a countdown clock
-      Author: 
-      Date:   
+      Author: Dylan Parisotto
+      Date:   8/3/2026
 
       Filename: project05-01.js
 */
-
+"use strict";
 // Constants to set the time given for the quiz in seconds
 // and the correct answers to each quiz question
-const quizTime = 20;
+const timeLimit = 90;
 const correctAnswers = ["10", "4", "-6", "5", "-7"];
 
 // Elements in the quiz page
@@ -21,33 +21,42 @@ let quizClock = document.getElementById("quizclock");
 let overlay = document.getElementById("overlay");
 
 // Initialize the quiz time
-quizClock.value = quizTime;
-let timeLeft = quizTime;
+quizClock.value = timeLimit;
+let timeLeft = timeLimit;
 
 // Declare the ID for timed commands
 // and the node list for questions
+let timeID;
+let questionList = document.querySelectorAll("div#quiz input");
+
+startQuiz.onclick = function() {
+   overlay.className = "showquiz";
+   timeID = setInterval(countdown, 1000);
+};
 
 
 
+function countdown() {
+   if (timeLeft === 0) {
+      clearInterval(timeID);
 
+      let totalCorrect = checkAnswers();
 
+      if (totalCorrect === correctAnswers.length) {
+         alert("Congratulations! You got 100%!");
+      } else {
+         alert("You got " + (correctAnswers.length - totalCorrect) + " incorrect answers out of " + correctAnswers.length + ".");
+      }
 
+      timeLeft = timeLimit;
+      quizClock.value = timeLeft;
+      overlay.className = "hidequiz";
+   } else {
+      timeLeft--;
+      quizClock.value = timeLeft;
+   }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*------------- Function to check the student answers ----------------*/
 function checkAnswers() {
    let correctCount = 0;
    
