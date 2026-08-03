bec6857e4f1b291243c4dae06c69325334d2284f
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-03 18:14:53 -0500

    hop 5-3

--- JavaScript/data-files/Chapter05/js05/project02/project05-02_txt.js
+++ JavaScript/data-files/Chapter05/js05/project02/project05-02_txt.js
@@ -4,9 +4,27 @@
       Project 05-02
 
       Project to move images between a photo bucket and photo list.
-      Author: 
-      Date:   
+      Author: Dylan Parisotto
+      Date:   8/3/2026--------------------------------------------------
 
       Filename: project05-02.js
 */
+
+var images = document.getElementsByTagName("img");
+var photoBucket = document.getElementById("photo_bucket");
+var photoList = document.getElementById("photo_list");
+
+for (var i = 0; i < images.length; i++) {
+   images[i].onclick = function() {
+      if (this.parentNode.id === "photo_bucket") {
+         var newItem = document.createElement("li");
+         photoList.appendChild(newItem);
+         newItem.appendChild(this);
+      } else {
+         var oldItem = this.parentNode;
+         photoBucket.appendChild(this);
+         oldItem.parentNode.removeChild(oldItem);
+      }
+   };
+}
 
