6f25f4cc2ac9769d95621ab7493ba1a10e2c0f42
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-03 18:27:58 -0500

    hop 5-4

--- JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
+++ JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
@@ -4,9 +4,33 @@
       Project 05-03
 
       Project to create a table of headings from an article
-      Author: 
-      Date:   
+      Author: Dylan Parisotto
+      Date:   8/3/2026
 
-      Filename: project05-03.js
+      Filename: project05-03_txt.js
 */
+
+let sourceDoc = document.getElementById("source_doc");
+let toc = document.getElementById("toc");
+let headingCount = 1;
+const heading = "H2";
+
+for (let n = sourceDoc.firstElementChild; n !== null; n = n.nextElementSibling) {
+   if (n.nodeName === heading) {
+      let anchor = document.createElement("a");
+      anchor.name = "doclink" + headingCount;
+      n.insertBefore(anchor, n.firstChild);
+
+      let listItem = document.createElement("li");
+      let link = document.createElement("a");
+      listItem.appendChild(link);
+
+      link.textContent = n.textContent;
+      link.href = "#doclink" + headingCount;
+
+      toc.appendChild(listItem);
+
+      headingCount++;
+   }
+}
 
