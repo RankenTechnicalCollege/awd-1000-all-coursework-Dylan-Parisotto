5ab89dd40621b90a5b10ccc36e43f2f907c47bde
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-03 12:15:58 -0500

    hop 4-3

--- JavaScript/data-files/Chapter04/js04/project03/project04-03_txt.js
+++ JavaScript/data-files/Chapter04/js04/project03/project04-03_txt.js
@@ -3,23 +3,25 @@
       Project 04-03
 
       Application to count the number of characters in a review comment
-      Author: 
-      Date:   
+      Author: Dylan Parisotto
+      Date:   8/3/2026
 
-      Filename: project04-03.js
+      Filename: project04-03_txt.js
 */
+
+ "use strict";
 
 // Maximum Length of Review
 const MAX_REVIEW = 100;
 document.getElementById("limit").innerHTML = MAX_REVIEW;
 
 // Reference to elemets in the web page
-wordCountBox = document.getElementById("countValue");
-warningBox = document.getElementById("warningBox");
+const wordCountBox = document.getElementById("countValue");
+const warningBox = document.getElementById("warningBox");
 
 
 // Event listener for typing into the comment box
-document.getElementById("comment").addEventListener(keyup, updateCount);
+document.getElementById("comment").addEventListener("keyup", updateCount);
 
 // Function to update the count with each keyup event
 function updateCount() {
@@ -27,9 +29,20 @@ function updateCount() {
    warningBox.innerHTML = "";
    
    // Count the number of characters in the comment box
-   commentText = document.getElementById("comment").value;
-   charCount = countCharacters(commentsText);
+   const commentText = document.getElementById("comment").value;
+   const charCount = countCharacters(commentText);
    
+   // Display the current character count, even if it exceeds the limit
+   wordCountBox.innerHTML = charCount;
+   
+   try {
+      if (charCount > MAX_REVIEW) {
+         throw "You have exceeded the character count limit.";
+      }
+   }
+   catch (error) {
+      warningBox.innerHTML = error;
+   }
 }
 
 
