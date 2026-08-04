a8495e96cc0ab8f4483d5f15b2dc11297db6c74e
Alisha_Staggs <Alisha_Staggs@insideranken.org>
2026-08-04 11:22:10 -0500

    hop 6-1

--- JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
+++ JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
@@ -4,9 +4,24 @@
       Project 06-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Dylan Parisotto
+      Date:   8/4/2026
 
       Filename: project06-01.js
 */
+
+var submitButton = document.getElementById("submitButton");
+var pwd = document.getElementById("pwd");
+var pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function() {
+   pwd.setCustomValidity("");
+   pwd2.setCustomValidity("");
+
+   if (!pwd.validity.valid) {
+      pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+   } else if (pwd.value !== pwd2.value) {
+      pwd2.setCustomValidity("Your passwords must match");
+   }
+});
 
