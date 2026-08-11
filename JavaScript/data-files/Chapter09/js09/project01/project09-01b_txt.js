fcf9d57436896a10eba71fdbaa96659e6d8826ea
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-11 17:50:17 -0500

    hop 9-1

--- JavaScript/data-files/Chapter09/js09/project01/project09-01b_txt.js
+++ JavaScript/data-files/Chapter09/js09/project01/project09-01b_txt.js
@@ -10,3 +10,17 @@
       Filename: project09-01b.js
 */
 
+let query = location.search.slice(1);
+query = query.replace(/\+/g, " ");
+query = decodeURIComponent(query);
+
+let cardFields = query.split("&");
+
+for (let i = 0; i < cardFields.length; i++) {
+   let nameValue = cardFields[i].split("=");
+   let name = nameValue[0];
+   let value = nameValue[1];
+
+   document.getElementById(name).textContent = value;
+}
+
