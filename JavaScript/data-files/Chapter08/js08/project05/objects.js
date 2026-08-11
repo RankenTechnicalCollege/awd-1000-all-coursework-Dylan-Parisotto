0b966ebc64bf1b462fd2e438d82c353818b0f198
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-11 16:51:58 -0500

    hop 8-5

--- JavaScript/data-files/Chapter08/js08/project05/objects.js
+++ JavaScript/data-files/Chapter08/js08/project05/objects.js
@@ -4,26 +4,26 @@
       Project 08-05
 
       Chess Objects used for Chess Games
-      Author: 
-      Date:   
+      Author: Dylan Parisotto
+      Date:   08/11/2026
 
-      Filename: objects_txt.js
+      Filename: objects.js
 */
 
 // Constructor Function for Chess Piece
 function piece(color, rank) {
-   this.color = color;
-   this.rank = rank;
-   this.square = null;
-   this.image = null;
+   this.color = color;    // the color of the piece (white or black)
+   this.rank = rank;      // the piece rank (Pawn, Knight, Bishop, Rook, Queen, King)
+   this.square = null;    // the square the piece occupies
+   this.image = null;     // stores the HTML code representing the image of the piece
 }
 
 // Constructor Function for a Chess Set
 function chessSet(game) {
-   this.pieces = [];
-
-   for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
+   this.pieces = []; // empty array of chess pieces used in the game 
+   
+   for (let i = 0; i < 8; i++) {         // loop through the chess board rows
+      for (let j = 0; j < 8; j++) {      // loop through the chess board columns
          if (game.board[i][j].length === 2) {
             let color = (game.board[i][j].charAt(0) === "B") ? "Black" : "White";
             let rank = "";
@@ -31,11 +31,12 @@ function chessSet(game) {
                case "P" : rank = "Pawn"; break;
                case "N" : rank = "Knight"; break;
                case "B" : rank = "Bishop"; break;
-               case "R" : rank = "Rook"; break;
+               case "R": rank = "Rook"; break;
                case "Q" : rank = "Queen"; break;
                case "K" : rank = "King"; break;
             }
-
+            
+            // Define a new chess piece
             let chessPiece = new piece(color, rank);
             chessPiece.square = String.fromCharCode(j + 97) + (8 - i);
             let image = "";
@@ -51,11 +52,12 @@ function chessSet(game) {
                case "WB" : image = "&#9815;"; break;
                case "WR" : image = "&#9814;"; break;
                case "WQ" : image = "&#9813;"; break;
-               case "WK" : image = "&#9812;"; break;
+               case "WK" : image = "&#9812;"; break;                  
             }
             chessPiece.image = image;
             this.pieces.push(chessPiece);
          }
       }
    }
+   
 }
--- JavaScript/data-files/Chapter08/js08/project05/project08-05_txt.js
+++ JavaScript/data-files/Chapter08/js08/project05/project08-05_txt.js
@@ -7,109 +7,89 @@
       Author: 
       Date:   
 
-      Filename: project08-05.js
+      Filename: project08-05_txt.js
 */
-
 
 /*--------------------- Interface Code -------------------*/
 
 // Interface Objects
-let moveLog = document.getElementById("moveLog");         // ol element containing the list of the moves
-let moveSpans = moveLog.getElementsByTagName("span");     // span element containing the individual moves
-let nextButton = document.getElementById("nextButton");   // button to move forward through the game
-let prevButton = document.getElementById("prevButton");   // button to move backward through the game
-let getLogButton = document.getElementById("getLog");     // button to retrieve game log stored in a JSON file 
-let blackBox = document.getElementById("blackBox");       // box containing captured black pieces
-let whiteBox = document.getElementById("whiteBox");       // box containing captured white pieces
-let titleBox = document.getElementById("title");          // h1 heading for game title
-let descBox = document.getElementById("description");     // paragraph for game description
+let moveLog = document.getElementById("moveLog");
+let moveSpans = moveLog.getElementsByTagName("span");
+let nextButton = document.getElementById("nextButton");
+let prevButton = document.getElementById("prevButton");
+let getLogButton = document.getElementById("getLog");
+let blackBox = document.getElementById("blackBox");
+let whiteBox = document.getElementById("whiteBox");
+let titleBox = document.getElementById("title");
+let descBox = document.getElementById("description");
 
 getLogButton.onchange = function() {
-   // Retrieve information about the selected file
-   let JSONfile = this.files[0];
-   
-   // Read the contents of the selected file
-   let fr = new FileReader();
-   fr.readAsText(JSONfile); 
+   const JSONfile = this.files[0];
+   const fr = new FileReader();
+   fr.readAsText(JSONfile);
 
-   // Once the file has finished loading, parse the JSON file
-   // and store the contents in the game object literal
-   fr.onload=function(){ 
-      // Load data from the JSON file into the game object
-      let game = json.parse(fr.result);
-      
+   fr.onload = function() {
+      const game = JSON.parse(fr.result);
+
       titleBox.textContent = game.title;
       descBox.textContent = game.description;
-      
+
       writeMoveLog(game.moves);
-      
-      // Create a new chess set object
-      let mySet = chessSet(game);
-      
+
+      const mySet = new chessSet(game);
       setupBoard(mySet);
-      
+
       nextButton.onclick = function() {
          if (game.move < game.moves.length - 1) {
-            showNextBoard(game);           
+            showNextBoard(game);
          }
-      }
+      };
+
       prevButton.onclick = function() {
          if (game.move > -1) {
-            showPrevBoard(game);           
+            showPrevBoard(game);
          }
-      }      
-   }
-   
+      };
+   };
 };
 
 function writeMoveLog(moves) {
-   for (let i = 0; i < moves.length; i+=2) {
+   for (let i = 0; i < moves.length; i += 2) {
       let newLI = document.createElement("li");
 
-      // add the white move for each round
       let whiteMove = document.createElement("span");
       whiteMove.textContent = moves[i];
       newLI.appendChild(whiteMove);
 
-      // add the black move for each round
       let blackMove = document.createElement("span");
-      blackMove.textContent = moves[i+1];   
-      newLI.appendChild(blackMove); 
+      blackMove.textContent = moves[i + 1];
+      newLI.appendChild(blackMove);
 
-      // Append the two moves to the ordered list
       moveLog.appendChild(newLI);
-   }   
+   }
 }
 
 function setupBoard(set) {
-   // Remove any pieces from the current board
    let piecesOnBoard = document.querySelectorAll("table#chessboard tr td span");
    for (let i = 0; i < piecesOnBoard.length; i++) {
       let parentCell = piecesOnBoard[i].parentElement;
       parentCell.removeChild(parentCell.lastElementChild);
    }
-   
-   // Place chess pieces on the board.
+
    for (let i = 0; i < set.pieces.length; i++) {
       let pieceImage = document.createElement("span");
       pieceImage.innerHTML = set.pieces[i].image;
       pieceImage.className = set.pieces[i].color;
       let chessSquare = document.getElementById(set.pieces[i].square);
       chessSquare.appendChild(pieceImage);
-   } 
+   }
 }
 
-
-/* Function to update the board when the next move is played */
 function showNextBoard(game) {
    game.move++;
-   
-   // Highlight the move text in the move log
    moveSpans[game.move].className = "highlight";
-   
-   // Read the notation for the next move 
    let moveStr = game.moves[game.move];
-   
+
    if (moveStr === "1-0") {
       window.alert("Black Resigns");
    } else if (moveStr === "0-1") {
@@ -127,101 +107,83 @@ function showNextBoard(game) {
    } else if (moveStr.includes("-")) {
       movePiece();
    }
-   
-   // Move the piece image from the starting cell to the ending cell
-   function moveCell(start, end) {    
-      document.getElementById(end).appendChild(document.getElementById(start).firstElementChild);      
+
+   function moveCell(start, end) {
+      document.getElementById(end).appendChild(document.getElementById(start).firstElementChild);
    }
-   
-   // Move the piece image into the cell and move the occupying piece back to the box
+
    function removeCell(cell) {
-      // Moves a captured piece to the box
-      if (game.move % 2 === 0) {    // white captured a black piece
+      if (game.move % 2 === 0) {
          blackBox.appendChild(document.getElementById(cell).firstElementChild);
-      } else {                      // black captured a white piece
+      } else {
          whiteBox.appendChild(document.getElementById(cell).firstElementChild);
       }
    }
-   
-   // Perform a kingside castle
+
    function kingSideCastle() {
-      if (game.move % 2 === 0) {  // white kingside castle
+      if (game.move % 2 === 0) {
          moveCell("e1", "g1");
          moveCell("h1", "f1");
-      } else {                    // black kingside castle
+      } else {
          moveCell("e8", "g8");
          moveCell("h8", "f8");
       }
    }
-   
-   // Perform a queenside castle
+
    function queenSideCastle() {
-      if (game.move % 2 === 1) {  // white queenside castle
+      if (game.move % 2 === 1) {
          moveCell("e1", "c1");
          moveCell("a1", "d1");
-      } else {                    // black queenside castle
+      } else {
          moveCell("e8", "c8");
          moveCell("a8", "d8");
       }
-   }  
-   
-   // Promote a pawn that reaches the end rank
+   }
+
    function pawnPromotion() {
       let mIndex = moveStr.indexOf("-");
-      let startCell = moveStr.substr(mIndex - 2,2);
+      let startCell = moveStr.substr(mIndex - 2, 2);
       let endCell = moveStr.substr(mIndex + 1, 2);
-      moveCell(startCell, endCell); 
+      moveCell(startCell, endCell);
       let newPiece = moveStr.charAt(moveStr.length - 1);
       let rankNum;
       switch (newPiece) {
-         case "P" : rankNum = 9817; break;  // unicode for white pawn
-         case "N" : rankNum = 9816; break;  // unicode for white knight
-         case "B" : rankNum = 9815; break;  // unicode for white bishop
-         case "R": rankNum = 9814; break;   // unicode for white rook
-         case "Q" : rankNum = 9813; break;  // unicode for white queen
-         case "K" : rankNum = 9812; break;  // unicode for white king
-      }   
-      if (game.move % 2 === 1) {   // move was made by black change unicode to black piece image
-         rankNum+=6;
+         case "P" : rankNum = 9817; break;
+         case "N" : rankNum = 9816; break;
+         case "B" : rankNum = 9815; break;
+         case "R" : rankNum = 9814; break;
+         case "Q" : rankNum = 9813; break;
+         case "K" : rankNum = 9812; break;
       }
-      
-      // Change image to promoted piece
+      if (game.move % 2 === 1) {
+         rankNum += 6;
+      }
       document.getElementById(endCell).firstElementChild.innerHTML = "&#" + rankNum + ";";
    }
-   
-   // Retrieve the address of starting cell and ending (occupied) cell
+
    function capturePiece() {
       let tIndex = moveStr.indexOf("x");
-      let startCell = moveStr.substr(tIndex - 2,2);
+      let startCell = moveStr.substr(tIndex - 2, 2);
       let endCell = moveStr.substr(tIndex + 1, 2);
-      removeCell(endCell);  // remove piece from the cell
-      moveCell(startCell, endCell)
-   }   
-   
-   // Retrieve the adress of the starting cell and ending (unoccupied) cell
+      removeCell(endCell);
+      moveCell(startCell, endCell);
+   }
+
    function movePiece() {
       let mIndex = moveStr.indexOf("-");
-      let startCell = moveStr.substr(mIndex - 2,2);
+      let startCell = moveStr.substr(mIndex - 2, 2);
       let endCell = moveStr.substr(mIndex + 1, 2);
-      moveCell(startCell, endCell)
+      moveCell(startCell, endCell);
    }
 }
 
-
-/* Function to update the board when the current move is undone */
 function showPrevBoard(game) {
-   // Remove highlighting from the move
    moveSpans[game.move].classList.remove("highlight");
-   
-   // Read the notation for of current move 
    let moveStr = game.moves[game.move];
-   
+
    if (moveStr === "1-0") {
-      // Do nothing in reverse
    } else if (moveStr === "0-1") {
-      // Do nothing in reverse
    } else if (moveStr === "1/2-1/2") {
-      // Do nothing in reverse
    } else if (moveStr === "0-0") {
       kingSideCastle();
    } else if (moveStr === "0-0-0") {
@@ -233,82 +195,70 @@ function showPrevBoard(game) {
    } else if (moveStr.includes("-")) {
       movePiece();
    }
-   
-   // Reduce the move number by 1
+
    game.move--;
-   
-   // Move the piece back from its ending cell to its starting cell
-   function moveCell(start, end) {    
-      document.getElementById(end).appendChild(document.getElementById(start).firstElementChild);      
+
+   function moveCell(start, end) {
+      document.getElementById(end).appendChild(document.getElementById(start).firstElementChild);
    }
-   
-   // Move a captured piece from its box back to the board
+
    function addCell(cell) {
-      // Moves a captured piece to the box
-      if (game.move % 2 === 0) {    // move the black piece back to the board
+      if (game.move % 2 === 0) {
          document.getElementById(cell).appendChild(blackBox.lastElementChild);
-      } else {                      // move the white piece back to the board
+      } else {
          document.getElementById(cell).appendChild(whiteBox.lastElementChild);
       }
    }
-   
-   // Perform a kingside caste in reverse
+
    function kingSideCastle() {
-      if (game.move % 2 === 0) {  // white kingside castle
+      if (game.move % 2 === 0) {
          moveCell("g1", "e1");
          moveCell("f1", "h1");
-      } else {                    // black kingside castle
+      } else {
          moveCell("g8", "e8");
          moveCell("f8", "h8");
       }
    }
-   
-   // Perform a queenside castle in reverse
+
    function queenSideCastle() {
-      if (game.move % 2 === 1) {  // white queenside castle
+      if (game.move % 2 === 1) {
          moveCell("c1", "e1");
          moveCell("d1", "a1");
-      } else {                    // black queenside castle
+      } else {
          moveCell("c8", "e8");
          moveCell("d8", "a8");
       }
-   }  
-   
-   // Demote a pawn that had reached the end rank
+   }
+
    function pawnDemotion() {
       let mIndex = moveStr.indexOf("-");
-      let startCell = moveStr.substr(mIndex + 1,2);
+      let startCell = moveStr.substr(mIndex + 1, 2);
       let endCell = moveStr.substr(mIndex - 2, 2);
-      moveCell(startCell, endCell); 
+      moveCell(startCell, endCell);
       let newPiece = moveStr.charAt(moveStr.length - 1);
-      let rankNum;   
-      if (game.move % 2 === 1) {   // move was made by black change unicode to black piece image
+      let rankNum;
+      if (game.move % 2 === 1) {
          rankNum = 9823;
       } else {
          rankNum = 9817;
       }
-      
-      // Change image to promoted piece
       document.getElementById(endCell).firstElementChild.innerHTML = "&#" + rankNum + ";";
-      
    }
-   
-   // Find the addresses of the starting and ending cell during a capture
+
    function addPiece() {
       let tIndex = moveStr.indexOf("x");
-      let startCell = moveStr.substr(tIndex + 1,2);
-      let endCell = moveStr.substr(tIndex -2, 2);
+      let startCell = moveStr.substr(tIndex + 1, 2);
+      let endCell = moveStr.substr(tIndex - 2, 2);
       moveCell(startCell, endCell);
       addCell(startCell);
-   }   
-   
-   // Find the addresses of the starting and ending cell during a move
+   }
+
    function movePiece() {
       let mIndex = moveStr.indexOf("-");
-      let startCell = moveStr.substr(mIndex + 1,2);
-      let endCell = moveStr.substr(mIndex -2, 2);
+      let startCell = moveStr.substr(mIndex + 1, 2);
+      let endCell = moveStr.substr(mIndex - 2, 2);
       moveCell(startCell, endCell);
-   }   
+   }
 }
 
 
