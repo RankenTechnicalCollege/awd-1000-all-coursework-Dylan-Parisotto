03afca137790751d8c6b282bd2c40a159f7d8ffb
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-11 11:36:15 -0500

    hop 7-3

--- JavaScript/data-files/Chapter07/js07/project03/project07-03_txt.js
+++ JavaScript/data-files/Chapter07/js07/project03/project07-03_txt.js
@@ -16,3 +16,26 @@ let hrsLeftBox = document.getElementById("hours");
 let minsLeftBox = document.getElementById("minutes");
 let secsLeftBox = document.getElementById("seconds");
 
+setInterval(countdown, 1000);
+
+countdown();
+
+function countdown() {
+      let now = new Date();
+      currentTime.textContent = now.toLocaleString();
+
+      let newYear = new Date("January 1, 2024");
+      let nextYear = now.getFullYear() + 1;
+      newYear.setFullYear(nextYear);
+
+      let daysLeft = (newYear - now) / (1000 * 60 * 60 * 24);
+      let hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
+      let minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60;
+      let secsLeft = (minsLeft - Math.floor(minsLeft)) * 60;
+
+      daysLeftBox.textContent = Math.floor(daysLeft);
+      hrsLeftBox.textContent = Math.floor(hrsLeft);
+      minsLeftBox.textContent = Math.floor(minsLeft);
+      secsLeftBox.textContent = Math.floor(secsLeft);
+}
+
