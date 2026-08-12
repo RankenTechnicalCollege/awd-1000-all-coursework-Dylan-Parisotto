44e61833e4906bf0314ef8d4b86272c814d6884e
Dylan-Parisotto <dylan_parisotto@insideranken.org>
2026-08-12 10:33:00 -0500

    hop 9-3

--- JavaScript/data-files/Chapter09/js09/project03/project09-03_txt.js
+++ JavaScript/data-files/Chapter09/js09/project03/project09-03_txt.js
@@ -15,5 +15,25 @@
 let lastVisitDate = document.getElementById("lastVisitDate");
 let articleDates = document.getElementsByClassName("posttime");
 
+if (localStorage.sbloggerVisit) {
+   let storedLastDate = localStorage.sbloggerVisit;
+   lastVisitDate.textContent = storedLastDate;
+   let lastDate = new Date(storedLastDate);
 
+   for (let i = 0; i < articleDates.length; i++) {
+      let articleDate = new Date(articleDates[i].textContent);
+      if (articleDate > lastDate) {
+         articleDates[i].innerHTML += " <strong>new</strong>";
+      }
+   }
+} else {
+   lastVisitDate.textContent = "Welcome to SBlogger!";
+
+   for (let i = 0; i < articleDates.length; i++) {
+      articleDates[i].innerHTML += " <strong>new</strong>";
+   }
+}
+
+let currentDate = new Date("9/12/2024");
+localStorage.sbloggerVisit = currentDate.toLocaleDateString();
 
